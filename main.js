71844d1a82be5395441d0286926dcd1a1b402d74
David Christa <davidchrista@gmail.com>
2022-12-09 12:47:43 +0100

    first working version

--- main.js
+++ main.js
@@ -1,19 +1,80 @@
-const { app, Tray, Menu, nativeImage } = require('electron')
+const { app, Tray, Menu, nativeImage } = require("electron");
+const fs = require("fs");
+const cp = require("child_process");
 
 let tray;
 
 app.whenReady().then(() => {
-  const icon = nativeImage.createFromPath("/usr/share/icons/server_off.png");
-  tray = new Tray(icon);
+  const offIcon = nativeImage.createFromPath("/usr/share/icons/server_off.png");
+  const onIcon = nativeImage.createFromPath("/usr/share/icons/server_on.png");
+  const statusFilePath = "/etc/server-status";
+  const wakeCommand = "/home/david/.bin/wakehome";
+  const shutdownCommand = "/home/david/.bin/shutdownhome";
+  const restartCommand = "/home/david/.bin/reboothome";
+  const timeout = 5000;
+
+  tray = new Tray(offIcon);
+
+  const startServer = () => {
+    cp.spawn(wakeCommand);
+  };
+
+  const stopServer = () => {
+    cp.spawn(shutdownCommand);
+  };
+
+  const restartServer = () => {
+    cp.spawn(restartCommand);
+  };
+
+  const checkStatus = () => {
+    fs.readFile(statusFilePath, "utf-8", (err, data) => {
+      if (err) {
+        tray.setImage(offIcon);
+      } else {
+        if (data === "UP") {
+          tray.setImage(onIcon);
+        } else {
+          tray.setImage(offIcon);
+        }
+      }
+    });
+  };
+
+  setInterval(() => {
+    checkStatus();
+  }, timeout);
 
   const contextMenu = Menu.buildFromTemplate([
-    { label: "Item1", type: "radio" },
-    { label: "Item2", type: "radio" },
-    { label: "Item3", type: "radio", checked: true },
-    { label: "Item4", type: "radio" },
+    {
+      label: "Start server",
+      type: "normal",
+      click: startServer,
+    },
+    {
+      label: "Stop server",
+      type: "normal",
+      click: stopServer,
+    },
+    {
+      label: "Restart server",
+      type: "normal",
+      click: restartServer,
+    },
+    {
+      label: "",
+      type: "separator",
+    },
+    {
+      label: "Quit",
+      type: "normal",
+      click: () => {
+        app.exit(0);
+      },
+    },
   ]);
 
   tray.setContextMenu(contextMenu);
-  tray.setToolTip("This is my application");
-  tray.setTitle("This is my title");
+  tray.setToolTip("Server manager");
+  tray.setTitle("Server manager");
 });
