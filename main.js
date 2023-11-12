6c53991523bbbca63fb312f5e92204ed9e7f997f
David Christa <davidchrista@gmail.com>
2023-11-12 12:05:13 +0100

    remove hard coded address

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const { app, Tray, Menu, nativeImage } = require("electron");
 const nf = require("node-fetch");
 const cp = require("child_process");
+const fs = require("fs");
 
 let tray;
 
 app.whenReady().then(() => {
   const offIcon = nativeImage.createFromPath("/usr/share/icons/server_off.png");
   const onIcon = nativeImage.createFromPath("/usr/share/icons/server_on.png");
-  const statusUrl =
-    "http://arbeglanretsc.duckdns.org/__servercheck__/check.txt";
+  const configFile = "/home/david/.bin/server-manager.json";
   const wakeCommand = "/home/david/.bin/wakehome";
   const shutdownCommand = "/home/david/.bin/shutdownhome";
   const restartCommand = "/home/david/.bin/reboothome";
   const timeout = 5000;
+
+  const configData = fs.readFileSync(configFile);
+  const configStr = configData.toString('utf8');
+  const config = JSON.parse(configStr);
 
   tray = new Tray(offIcon);
 
@@ -29,7 +33,7 @@ app.whenReady().then(() => {
   };
 
   const checkStatus = () => {
-    nf(statusUrl)
+    nf(config.address)
       .then((res) => res.text())
       .then((text) => {
         if (text === "online\n") {
@@ -79,4 +83,6 @@ app.whenReady().then(() => {
   tray.setContextMenu(contextMenu);
   tray.setToolTip("Server manager");
   tray.setTitle("Server manager");
+
+  checkStatus();
 });
